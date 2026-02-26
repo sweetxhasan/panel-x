f695dc83991e27f9654f99516a15bd4b859d5947
sweetxhasan <hasanking25809560@gmail.com>
2026-02-26 13:25:10 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'jost': ['Jost', 'sans-serif'],
+      },
+      borderRadius: {
+        'none': '0',
+      }
+    },
+  },
+  plugins: [],
+}
